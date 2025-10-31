74abe3369e2b011a780462fc1ccf61451e526a66
ryouma21 <zuotenglengzhen58@gmail.com>
2025-10-31 18:35:41 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   root to: "rooms#index"
   devise_for :users
-  resources :rooms, only: [:new, :create, :index, :show] do
+  resources :rooms, only: [:new, :create, :show, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
