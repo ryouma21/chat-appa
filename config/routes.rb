a556014a54710ea523bc62ccc8224b65907f1034
ryouma21 <zuotenglengzhen58@gmail.com>
2025-10-31 17:15:52 +0900

    チャット画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create, :index]
   root to: "messages#index"
   devise_for :users
 end
