b6c08b82fe7ef8e482d32f810bec493a87c476d1
ryouma21 <zuotenglengzhen58@gmail.com>
2025-10-31 17:18:08 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create, :index]
-  root to: "messages#index"
+  root to: "rooms#index"
   devise_for :users
 end
